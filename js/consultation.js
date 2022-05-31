e303e39bd93a4bd5d49997fd74d16772d7d299b4
limsongeun94 <cocorosaya@naver.com>
2022-05-31 15:43:49 +0900

    ..

--- js/consultation.js
+++ js/consultation.js
@@ -72,3 +72,28 @@ function handlePhoneNumberInput(evt) {
     // ================================
   }
 }
+
+// 한글완성
+function handleKrCompleteNumInput(evt) {
+  const inputStr = evt.target.value;
+  const hint = evt.target.nextElementSibling;
+
+  // 입력이 존재할 경우에만 실행
+  if (inputStr) {
+    // 잘못된 입력 검출 ===================
+    const reg = /^[가-힣|0-9]+$/;
+    if (!reg.test(inputStr)) {
+      hint.style.color = "#c80029";
+      hint.innerText = "잘못된 입력입니다.";
+    } else {
+      hint.style.color = "#645959";
+      hint.innerText = "* 한글과 숫자만 입력하세요.";
+    }
+    // ================================
+
+    // 잘못된 입력 교정 ===================
+    const result = inputStr.replace(/[^ㄱ-ㅎ|가-힣|0-9]/g, "");
+    evt.target.value = result;
+    // ================================
+  }
+}
