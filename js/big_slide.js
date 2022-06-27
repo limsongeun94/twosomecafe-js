fea936686e0b550664e317750b156eed58616582
limsongeun94 <cocorosaya@naver.com>
2022-06-27 15:17:16 +0900

    ...

--- js/big_slide.js
+++ js/big_slide.js
@@ -1,112 +1,65 @@
-const banner = document.getElementById("main_banner");
-const imgArr = [
+const bannerArr = [
   "img/index/main_banner/paint.jpg",
   "img/index/main_banner/dessert.jpg",
+  "img/index/main_banner/cake.jpg",
   "img/index/main_banner/beverage.jpg",
   "img/index/main_banner/towsome_heart.jpg",
-  "img/index/main_banner/cake.jpg",
 ];
 
-function viewBanner(arr) {
-  document.getElementById("main_banner").setAttribute("src", imgArr[0]);
+//----------------------------------------html생성----------------------------------------
+// bContainer 생성
+let bContainer = document.createElement("div");
+// bContainer에 id 부여
+bContainer.id = "banner_container";
+
+// inner에 img파일 추가 X5
+for (i = 0; i < bannerArr.length; i++) {
+  // bImg 생성
+  let bImg = document.createElement("img");
+  // bImg에 진짜 img 꽂아 넣기
+  bImg.setAttribute("src", bannerArr[i]);
+
+  // bInner 생성
+  bInner = document.createElement("div");
+  // bInner 에 id부여
+  bInner.id = "banner_inner_" + i;
+  // bInner 에 class부여
+  bInner.className = "banner_inner";
+  // Binner에 bImg파일 추가
+  bInner.appendChild(bImg);
+
+  // #banner_container에 .banner_inner 추가
+  bContainer.appendChild(bInner);
 }
 
-const imgLt = document.getElementById("btn_left");
-const imgRt = document.getElementById("btn_rignt");
+// .banner_wrapper에 #banner_container 추가
+document.getElementsByClassName("banner_wrapper")[0].appendChild(bContainer);
+//----------------------------------------html생성----------------------------------------
 
-leftBtn.addEventListener("click", () => currentImgArray(-1));
-rightBtn.addEventListener("click", () => currentImgArray(1));
+// for (i = 0; i < 5; i++) {
+//   bContainer.style.transform = "translate(-100vw)";
+//   // i % 5 * 100
+// }
 
-function currentImgArray(dir) {
-  if (dir > 0) {
-    imgArr = imgArr.slice(1).concat(slice(0, 1));
-  } else if (dir < 0) {
-    imgArr = imgArr.slice(-1).concat(slice(0, 3));
-  } else {
-  }
+let bBTN0 = document.getElementById("btn0");
+let bBTN1 = document.getElementById("btn1");
+let bBTN2 = document.getElementById("btn2");
+let bBTN3 = document.getElementById("btn3");
+let bBTN4 = document.getElementById("btn4");
 
-  viewBanner(imgArr.slice(0));
+// bBTN0.addEventListener("click", () => {
+//   bContainer.style.transform = "translate(0vw)";
+//   console.log("안녕0번");
+// });
+
+// bBTN1.addEventListener("click", () => {
+//   bContainer.style.transform = "translate(-100vw)";
+//   console.log("안녕1번");
+// });
+
+const arr = document.getElementsByClassName("num_btn");
+for (let i = 0; i <= 4; i++) {
+  arr[i].addEventListener("click", () => {
+    bContainer.style.transform = `translate(-${(i % 5) * 100}vw)`;
+  });
 }
-
-viewBanner(imgArr.slice(0));
-// banner.setAttribute("src", imgArr[0]);
-
-// vimg = 0;
-// function next_img() {
-//   if (vimg < imgArr.length - 1) {
-//     banner.setAttribute("src", imgArr[++vimg]);
-//   } else if (vimg == imgArr.length - 1) {
-//     vimg = 0;
-//     banner.setAttribute("src", imgArr[0]);
-//   }
-
-//   updateIndicator();
-// }
-
-// function prev_img() {
-//   if (vimg > 0) {
-//     banner.setAttribute("src", imgArr[--vimg]);
-//   } else if (vimg == 0) {
-//     vimg = imgArr.length - 1;
-//     banner.setAttribute("src", imgArr[imgArr.length - 1]);
-//   }
-
-//   updateIndicator();
-// }
-
-// const btn0 = document.getElementById("btn0");
-// const btn1 = document.getElementById("btn1");
-// const btn2 = document.getElementById("btn2");
-// const btn3 = document.getElementById("btn3");
-// const btn4 = document.getElementById("btn4");
-
-// btn0.addEventListener("click", () => {
-//   banner.setAttribute("src", imgArr[0]);
-//   updateIndicator();
-// });
-// btn1.addEventListener("click", () => {
-//   banner.setAttribute("src", imgArr[1]);
-//   updateIndicator();
-// });
-// btn2.addEventListener("click", () => {
-//   banner.setAttribute("src", imgArr[2]);
-//   updateIndicator();
-// });
-// btn3.addEventListener("click", () => {
-//   banner.setAttribute("src", imgArr[3]);
-//   updateIndicator();
-// });
-// btn4.addEventListener("click", () => {
-//   banner.setAttribute("src", imgArr[4]);
-//   updateIndicator();
-// });
-
-// function updateIndicator() {
-//   let bannerSRC = banner.getAttribute("src");
-//   btn0.innerText = "○";
-//   btn1.innerText = "○";
-//   btn2.innerText = "○";
-//   btn3.innerText = "○";
-//   btn4.innerText = "○";
-//   switch (bannerSRC) {
-//     case "img/index/main_banner/paint.jpg":
-//       btn0.innerText = "●";
-//       break;
-//     case "img/index/main_banner/dessert.jpg":
-//       btn1.innerText = "●";
-//       break;
-//     case "img/index/main_banner/beverage.jpg":
-//       btn2.innerText = "●";
-//       break;
-//     case "img/index/main_banner/towsome_heart.jpg":
-//       btn3.innerText = "●";
-//       break;
-//     case "img/index/main_banner/cake.jpg":
-//       btn4.innerText = "●";
-//       break;
-//   }
-
-//   runInterval();
-// }
-
-// setInterval(next_img, 3000);
