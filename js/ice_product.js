7061886b459cf2b30e97ca79f58bd77fba09fe51
limsongeun94 <cocorosaya@naver.com>
2022-07-24 00:22:05 +0900

    아이스크림 화면출력 완료

--- js/ice_product.js
+++ js/ice_product.js
@@ -1,31 +1,58 @@
+let data = null;
+
 axios
-	.get("http://twosome-api.seoly.me")
-	.then(res => {
-		const data = res.data
+  .get("http://twosome-api.seoly.me/api/product", {
+    params: {
+      main_filter: "ice_cream",
+    },
+  })
+  .then((res) => {
+    const data = res.data;
+    const ice_wrap = document.getElementById("ice_item_wrap");
+    draw(ice_wrap, data);
+  });
 
-		const ice = data.filter(x => x.main_menu == "ice_cream")
+function draw(target, item) {
+  let newTarget = target.cloneNode();
+  let parentNode = target.parentNode;
+  parentNode.appendChild(newTarget);
+  target.remove();
 
-		const bev_wrap = document.getElementById("ice_item_wrap")
-		ice.map((x) => {
-			let inner = document.createElement("div")
-			let img = document.createElement("img")
-			let text = document.createElement("p")
-			
-			inner.className = "inner"
-			img.setAttribute("src", x.img)
-			text.innerText = x.name
-			
-			inner.appendChild(img)
-			inner.appendChild(text)
-			bev_wrap.appendChild(inner)
-		})
-		
-        
-	})
-function showclick() {
-console.log("클릭")
-}
+  item.forEach((x) => {
+    let inner = document.createElement("div");
+    let img = document.createElement("img");
+    let text = document.createElement("p");
+    let layer = document.createElement("div");
+    let layer_inner = document.createElement("div");
+    let layer_name = document.createElement("h2");
+    let layer_line = document.createElement("hr");
+    let layer_explain = document.createElement("p");
 
-function showchange() {
-console.log("체인지")
+    inner.className = "inner";
+    img.setAttribute("src", x.img);
+    text.innerText = x.name;
+    layer.className = "layer";
+    layer_inner.className = "layer_inner";
+    layer_name.innerText = x.name;
+    layer_explain.innerText = x.text;
+
+    inner.appendChild(img);
+    inner.appendChild(text);
+    inner.appendChild(layer);
+    layer.appendChild(layer_inner);
+    layer_inner.appendChild(layer_name);
+    layer_inner.appendChild(layer_line);
+    layer_inner.appendChild(layer_explain);
+    newTarget.appendChild(inner);
+
+    inner.addEventListener("click", () => {
+      if (layer.style.opacity == 0) {
+        layer.style.opacity = 1;
+        layer_inner.style.opacity = 1;
+      } else if (layer.style.opacity == 1) {
+        layer.style.opacity = 0;
+        layer_inner.style.opacity = 0;
+      }
+    });
+  });
 }
